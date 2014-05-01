ef2b49428d254f0ee2ba55e030c94e837f59ccf9
Harold Fritsch <fritsc_h@epitech.eu>
2014-05-01 19:28:27 +0200

    parsing des options corriges

--- src/main.c
+++ src/main.c
@@ -5,16 +5,22 @@
 ** Login   <fritsc_h@epitech.net>
 ** 
 ** Started on  Thu May  1 18:27:53 2014 Harold Fritsch
-** Last update Thu May  1 18:55:57 2014 Harold Fritsch
+** Last update Thu May  1 19:23:24 2014 Harold Fritsch
 */
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-void	ptrace_exec(char *program_name)
+void	ptrace_exec(char **av)
 {
-  printf("ptrace_exec(%s)\n", program_name);;
+  if (execvp(av[1], &av[1]) == -1)
+    {
+      fprintf(stderr, "Error : failed to execute '%s'\n", av[2]);
+      return ;
+    }
+  printf("ptrace_exec(%s)\n", av[2]);
 }
 
 void	ptrace_attach(int pid)
@@ -24,15 +30,15 @@ void	ptrace_attach(int pid)
       fprintf(stderr, "ERROR : invalid PID\n");
       return ;
     }
-  printf("ptrace_attach(%d)\n", pid);;
+  printf("ptrace_attach(%d)\n", pid);
 }
 
 int	main(int ac, char **av)
 {
-  if (ac == 2)
-    ptrace_exec(av[1]);
-  else if (ac == 3 && strcmp("-p", av[1]))
+  if (ac == 3 && strcmp("-p", av[1]) == 0)
     ptrace_attach(atoi(av[2]));
+  else if (ac >= 2)
+    ptrace_exec(av);
   else
     fprintf(stderr, "USAGE : %s [-p PID] | program name\n", av[0]);
   return (0);
